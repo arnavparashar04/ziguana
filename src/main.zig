cb65d3733c5d29f855b36b24f268507ea21601bb
Arnav Parashar <arnavparashar408@gmail.com>
2026-07-19 17:24:28 +0530

    feat: add support for compiler command line flags and passing path

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,27 @@ const std = @import("std");
 const lexerMod = @import("lexer.zig");
 const fetcher = @import("fetcher.zig");
 const parser = @import("parser.zig");
+const cli = @import("cli.zig");
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-
-    const path: []const u8 = ""; // handle this later
-
-    const source = try fetcher.readSource(io, init.gpa, path);
-    defer init.gpa.free(source);
-
+    const args = try cli.parseArgs(init);
+    if (args.ask_help or args.ask_version) {
+        return;
+    }
+    const source = try fetcher.readSource(io, init.gpa, args.path);
     var lexer = lexerMod.Lexer.init(source);
+    var tokens = try lexer.lex(init.gpa);
 
-    var tokens = try lexerMod.Lexer.lex(&lexer, init.gpa);
+    if (args.token_print) {
+        // print tokens
+    }
+    var p = parser.Parser.init(init.gpa, tokens.items);
+    const program = try p.parse();
+    _ = program;
+    if (args.ast_print) {
+        // ast printer
+    }
+    defer init.gpa.free(source);
     defer tokens.deinit(init.gpa);
 }
